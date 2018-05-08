ece3a46d7ae8db308889fe5136e463bd672b968c
Lucas McDonald <lucmcd10@gmail.com>
2018-05-08 13:12:53 -0400

    First

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PersonalSite
 Personal website for learning and practice.
+
+Author: Lucas McDonald
+Date Created: 5/8/2018
---
+++ sources/app.js
@@ -0,0 +1,6 @@
+function showPhoto() {
+    document.getElementById('photo').src='.\photos\winter.jfif';
+}
+function hidePhoto() {
+    document.getElementById('photo').src='.\photos\summer.jfif';
+}
