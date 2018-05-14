dabb9b0d03fad09e57af3c06db4e72fd4ec5ebc0
Lucas McDonald <lucmcd10@gmail.com>
2018-05-14 09:28:09 -0400

    Template

--- sources/app.js
+++
@@ -1,6 +0,0 @@
-function showPhoto() {
-    document.getElementById('photo').src='.\photos\winter.jfif';
-}
-function hidePhoto() {
-    document.getElementById('photo').src='.\photos\summer.jfif';
-}
